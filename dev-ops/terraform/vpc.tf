6000040f829f2bdd34c9a4541954250d12a3660f
yzhbankov <yzhbankov@redpointpositioning.com>
2024-04-15 23:54:07 +0000

    Make subnets public

--- dev-ops/terraform/vpc.tf
+++ dev-ops/terraform/vpc.tf
@@ -3,21 +3,24 @@ resource "aws_vpc" "web_server_vpc" {
 }
 
 resource "aws_subnet" "subnet_a" {
-  vpc_id            = aws_vpc.web_server_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a" # Change to your desired AZ
+  vpc_id                  = aws_vpc.web_server_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a" # Change to your desired AZ
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet_b" {
-  vpc_id            = aws_vpc.web_server_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b" # Change to your desired AZ
+  vpc_id                  = aws_vpc.web_server_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b" # Change to your desired AZ
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet_c" {
-  vpc_id            = aws_vpc.web_server_vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1c" # Change to your desired AZ
+  vpc_id                  = aws_vpc.web_server_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1c" # Change to your desired AZ
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "internet_gw" {

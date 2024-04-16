de1a5e3465df477c8ab8271fed20b15344f6883c
yzhbankov <yzhbankov@redpointpositioning.com>
2024-04-16 02:51:15 +0000

    Fix internet connectivity from public subnets

--- dev-ops/terraform/security_groups.tf
+++ dev-ops/terraform/security_groups.tf
@@ -1,7 +1,7 @@
-resource "aws_security_group" "web_server_sg" {
-  name        = "${terraform.workspace}-yz-web-server-sg"
-  description = "Security group for web servers"
-  vpc_id      = aws_vpc.web_server_vpc.id
+resource "aws_security_group" "alb_sg" {
+  name        = "${terraform.workspace}-yz-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 80
@@ -25,16 +25,37 @@ resource "aws_security_group" "web_server_sg" {
   }
 }
 
+resource "aws_security_group" "asg_sg" {
+  name        = "${terraform.workspace}-yz-asg-sg"
+  description = "Security group for ASG"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "efs_sg" {
   name        = "${terraform.workspace}-yz-efs-sg"
   description = "Security group for EFS"
-  vpc_id      = aws_vpc.web_server_vpc.id
+  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 2049
     to_port     = 2049
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.web_server_vpc.cidr_block]
+    cidr_blocks = [aws_vpc.my_vpc.cidr_block]
   }
 
   egress {

d9482cc5f00ba71ac0e8715c41d5ff41c6f6c79d
yzhbankov <yzhbankov@redpointpositioning.com>
2024-04-16 03:08:13 +0000

    Fix internet connectivity from public subnets

--- dev-ops/terraform/vpc.tf
+++ dev-ops/terraform/vpc.tf
@@ -5,21 +5,21 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet_a" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a" # Change to your desired AZ
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet_b" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b" # Change to your desired AZ
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet_c" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1b" # Change to your desired AZ
+  availability_zone       = "us-east-1c"
   map_public_ip_on_launch = true
 }
 

54b1778d51898fc720cb7d92b610a6702a26c926
yzhbankov <yzhbankov@redpointpositioning.com>
2024-04-17 01:07:03 +0000

    Add CloudFront destribution

--- dev-ops/terraform/main.tf
+++ dev-ops/terraform/main.tf
@@ -148,10 +148,10 @@ resource "aws_cloudfront_distribution" "distribution" {
     origin_id   = "${terraform.workspace}-yz-origin-id"
 
     custom_origin_config {
-      http_port              = 443
-      https_port             = 443
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
+      http_port              = 80
+      https_port             = 80
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = []
     }
   }
 
